c6964635d1815b4957156a648654f7aff570a89a
Hendri Alqori <teamhendri18@gmail.com>
2024-10-25 15:45:16 +0700

    feat: add download progress indicator

--- src/modules/claim/download-file.tsx
+++ src/modules/claim/download-file.tsx
@@ -1,15 +1,28 @@
 "use client";
 
 import React from "react";
-import axios from "axios";
+import axios, { AxiosProgressEvent } from "axios";
+import { AnimatePresence, motion } from "framer-motion";
 import { API } from "#/constants";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
+const statusMap = {
+    loading: false,
+    success: false,
+    error: false
+}
+
 export function DownloadFile({ encrypted }: { encrypted: string }) {
     const router = useRouter()
+    const [status, setStatus] = React.useState(statusMap)
+    const [progressValue, setProgressValue] = React.useState(0)
 
-    function download(blob: Blob, filename: string) {
+    function statusHandler(map: Partial<Record<keyof typeof statusMap, boolean>>) {
+        setStatus(({ ...statusMap, ...map }))
+    }
+
+    function downloadFile(blob: Blob, filename: string) {
         // create object url
         const file = URL.createObjectURL(blob)
 
@@ -27,7 +40,7 @@ export function DownloadFile({ encrypted }: { encrypted: string }) {
         // Clean up
         document.body.removeChild(anchor)
 
-        router.push("/")
+        // router.push("/")
 
         toast.success("Berhasil mendownload produk")
         setTimeout(() => {
@@ -35,25 +48,41 @@ export function DownloadFile({ encrypted }: { encrypted: string }) {
         }, 3000)
     }
 
+
+    function downloadProgress(event: AxiosProgressEvent) {
+        const progress = event.bytes
+        setProgressValue(progress)
+    }
+
     const requestAPI = React.useCallback(async () => {
+        statusHandler({ loading: true })
         try {
             const res = await axios.get(`${API}/download/${encrypted}`, {
-                responseType: "blob"
+                responseType: "blob",
+                onDownloadProgress: downloadProgress
+
             })
             const blob = res.data as Blob
-            
+
             // eslint-disable-next-line
             //@ts-ignore
             const filename = res.headers.get("X-Filename")
-            download(blob, filename)
+            downloadFile(blob, filename)
+
+            statusHandler({ loading: true, success: true })
 
         } catch (error) {
             console.error(error)
+            statusHandler({ error: true })
+
             router.push("/404")
+
+        } finally {
+            statusHandler({ loading: false })
         }
 
         // eslint-disable-next-line
-    }, [])
+    }, [status])
 
     React.useEffect(() => {
         requestAPI()
@@ -62,6 +91,28 @@ export function DownloadFile({ encrypted }: { encrypted: string }) {
     }, [])
 
     return (
-        <section aria-label="download-file" />
+        <section className="relative min-h-dvh w-full font-mulish" aria-label="download-file">
+            <AnimatePresence>
+                {status.loading && (
+                    <motion.div
+                        initial={{ top: "2.5rem", opacity: 0 }}
+                        animate={{ top: "1.25rem", opacity: 1 }}
+                        exit={{ top: "-1.25rem", opacity: 0 }}
+                        transition={{ duration: 0.5, bounce: false }}
+                        className="max-w-[400px] absolute top-5 right-5 rounded-lg shadow-lg bg-slate-50 p-4"
+                    >
+                        <div className="flex flex-col md:flex-row justify-between items-start gap-3 md:gap-5">
+                            <div>
+                                <h1 className="font-semibold text-sm md:text-base">Hampir sampai</h1>
+                                <p className="text-xs md:text-sm text-gray-500">pengunduhan file sedang berlangsung, jangan tutup halaman anda</p>
+                            </div>
+                            <div>
+                                <p className="text-[0.65rem] text-gray-700">{progressValue} bytes</p>
+                            </div>
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </section>
     )
 }
